33439cf7eaaeefcd3e76deba1cbecaa90674a3ec
Yiming Gong <277645526@qq.com>
2022-05-24 17:17:06 -0400

    fix path

--- jfr-action/entrypoint.sh
+++ jfr-action/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-/app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$2"
+java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$2"
 
 /app/bin/jenkinsfile-runner-launcher "$1" -w /app/jenkins/jenkins.war -p "$2" -f "$3"
