3d56bf095113fc42e83c78db71356d8d8626cef9
Yiming Gong <277645526@qq.com>
2022-05-24 17:57:12 -0400

    test old war

--- jfr-action/entrypoint.sh
+++ jfr-action/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf /usr/share/jenkins/ref/plugins
 echo "p1"
 #unzip /app/jenkins-${JENKINS_VERSION}.war -d /app/jenkins-${JENKINS_VERSION}
-unzip /app/jenkins/jenkins.war -d /app/jenkins
+#unzip /app/jenkins/jenkins.war -d /app/jenkins
 echo "p2"
 java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
 echo "p3"
