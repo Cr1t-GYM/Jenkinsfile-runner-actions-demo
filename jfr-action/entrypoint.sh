af0ce97550e7b4b130704cb2253cf8de8bc4688b
Yiming Gong <277645526@qq.com>
2022-05-24 17:36:25 -0400

    add latest war package

--- jfr-action/entrypoint.sh
+++ jfr-action/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
-ls -lrt /usr/share/jenkins/ref/plugins
-/app/bin/jenkinsfile-runner-launcher "$1" -w /app/jenkins/jenkins.war -p /usr/share/jenkins/ref/plugins -f "$3"
+unzip /app/jenkins-${JENKINS_VERSION}.war -d /app/jenkins-${JENKINS_VERSION}
+
+java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins-${JENKINS_VERSION} --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
+
+/app/bin/jenkinsfile-runner-launcher "$1" -w /app/jenkins-${JENKINS_VERSION} -p /usr/share/jenkins/ref/plugins -f "$3"
