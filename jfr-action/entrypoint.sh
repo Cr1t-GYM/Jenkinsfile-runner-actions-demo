a883181cc9b4fa8f061e5c50da416ba12deec17e
Yiming Gong <277645526@qq.com>
2022-05-25 11:39:49 -0400

    update plugin

--- jfr-action/entrypoint.sh
+++ jfr-action/entrypoint.sh
@@ -2,11 +2,9 @@
 
 set -e
 #rm -rf /usr/share/jenkins/ref/plugins
-echo "p0"
-java -jar /app/bin/jenkins-plugin-manager.jar --available-updates --war /app/jenkins-${JENKINS_VERSION}.war
-echo "p1"
-unzip /app/jenkins-${JENKINS_VERSION}.war -d /app/jenkins-${JENKINS_VERSION}
-echo "p2"
-java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins-${JENKINS_VERSION}.war --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
+#echo "p1"
+#unzip /app/jenkins-${JENKINS_VERSION}.war -d /app/jenkins-${JENKINS_VERSION}
+#echo "p2"
+#java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins-${JENKINS_VERSION}.war --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
 echo "p3"
-/app/bin/jenkinsfile-runner-launcher "$1" -w /app/jenkins-${JENKINS_VERSION} -p /usr/share/jenkins/ref/plugins -f "$3"
+/app/bin/jenkinsfile-runner-launcher "$1" -w /app/jenkins -p /usr/share/jenkins/ref/plugins -f "$3"
