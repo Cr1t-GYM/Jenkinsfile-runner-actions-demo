9b6e665bdb3c21b42b59d6b607fb482d63a2469c
Yiming Gong <277645526@qq.com>
2022-05-25 11:46:44 -0400

    update plugin

--- jfr-action/entrypoint.sh
+++ jfr-action/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 #rm -rf /usr/share/jenkins/ref/plugins
 #echo "p1"
 #unzip /app/jenkins-${JENKINS_VERSION}.war -d /app/jenkins-${JENKINS_VERSION}
-#echo "p2"
-#java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins-${JENKINS_VERSION}.war --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
+echo "p2"
+java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
 echo "p3"
 /app/bin/jenkinsfile-runner-launcher "$1" -w /app/jenkins -p /usr/share/jenkins/ref/plugins -f "$3"
