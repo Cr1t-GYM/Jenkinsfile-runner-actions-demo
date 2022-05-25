d7f152f73768d6678c4ccc5ff8cbe30968444e67
Yiming Gong <277645526@qq.com>
2022-05-25 12:02:07 -0400

    final:use self-contained war&not install plugins

--- jfr-action/entrypoint.sh
+++ jfr-action/entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 #unzip /app/jenkins-${JENKINS_VERSION}.war -d /app/jenkins-${JENKINS_VERSION}
 #TODO: download latest plugins/minimum required version plugins will cause exception.
 #TODO: use the self-contained war will cause exception but can run the pipeline.
-echo "p2"
-java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
+#echo "p2"
+#java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$2" --plugin-download-directory=/usr/share/jenkins/ref/plugins
 echo "p3"
 /app/bin/jenkinsfile-runner-launcher "$1" -w /app/jenkins -p /usr/share/jenkins/ref/plugins -f "$3"
